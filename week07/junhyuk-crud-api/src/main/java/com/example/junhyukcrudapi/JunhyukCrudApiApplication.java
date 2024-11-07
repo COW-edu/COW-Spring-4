cc29252442de874253343ae23daca24c79a8a6d2
usernamejunhyuk <leejunhyuk112@gmail.com>
2024-11-07 18:47:24 +0900

    chore: 초기 문서 설정

---
+++ week07/junhyuk-crud-api/src/main/java/com/example/junhyukcrudapi/JunhyukCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.junhyukcrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunhyukCrudApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunhyukCrudApiApplication.class, args);
+    }
+
+}
