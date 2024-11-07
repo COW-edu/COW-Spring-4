1cb1fbbd09c0d64d37e7d59821b857686ed7a0e7
enohs <kmsn01@naver.com>
2024-11-07 18:48:37 +0900

    chore : 초기 문서 설정

---
+++ week07/kimsion-crud-api/src/main/java/practice/kimsioncrudapi/KimsionCrudApiApplication.java
@@ -0,0 +1,13 @@
+package practice.kimsioncrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimsionCrudApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KimsionCrudApiApplication.class, args);
+  }
+
+}
