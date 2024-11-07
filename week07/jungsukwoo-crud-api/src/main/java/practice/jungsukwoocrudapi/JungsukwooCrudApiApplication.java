08b4b4284059bda917ddea23e55c3267d8290926
Jungsukwoo <jungsw1106@naver.com>
2024-11-07 18:57:28 +0900

    chore : 초기문서 설정

---
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/JungsukwooCrudApiApplication.java
@@ -0,0 +1,13 @@
+package practice.jungsukwoocrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungsukwooCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungsukwooCrudApiApplication.class, args);
+	}
+
+}
