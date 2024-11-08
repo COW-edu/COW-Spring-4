b7b4fd9236d371adf6bb6077a363e3b2396b2c03
나경호 <hoyana1225@gmail.com>
2024-11-08 10:46:05 +0900

    chore: init study project

---
+++ src/main/java/cow/hoyajpaapi/HoyaJpaApiApplication.java
@@ -0,0 +1,13 @@
+package cow.hoyajpaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoyaJpaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoyaJpaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cow/hoyajpaapi/HoyaJpaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cow.hoyajpaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoyaJpaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
