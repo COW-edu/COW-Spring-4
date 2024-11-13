c4e96704c08230445cb3491e38a8ab61a3e7ea2b
Jungsukwoo <jungsw1106@naver.com>
2024-11-14 00:40:29 +0900

    업데이트를 위한 updateinfo 메소드 생성

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/member/Member.java
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/member/Member.java
@@ -2,14 +2,12 @@ package practice.jungsukwoocrudapi.member;
 
 import lombok.Builder;
 import lombok.Getter;
-//이 코드에서 게터 어노테이션 어떻게 작동하는건지 확인 필요, 걍 전체적으로 어노테이션 역할 확인 요망
+
 @Getter
 public class Member {
 
     private String name;
-
     private String email;
-
     public String password;
 
     @Builder
@@ -17,5 +15,10 @@ public class Member {
         this.name = name;
         this.email = email;
         this.password = password;
+    }
+
+    public void updateInfo(String name, String email) {
+        this.name = name;
+        this.email = email;
     }
 }
