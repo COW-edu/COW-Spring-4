12305e301ff23a00774f1935ded8c900ca151e51
Jungsukwoo <jungsw1106@naver.com>
2024-11-14 00:32:42 +0900

    업데이트, 조회, 삭제, 예외처리

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/member/MemberController.java
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/member/MemberController.java
@@ -1,9 +1,10 @@
 package practice.jungsukwoocrudapi.member;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import practice.jungsukwoocrudapi.member.exception.MemberNotFoundException;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,7 +13,29 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/members")
-    public void signUp(@RequestBody CreateMemberRequest createMemberRequest){
+    public void signUp(@RequestBody CreateMemberRequest createMemberRequest) {
         memberService.signUp(createMemberRequest);
     }
+
+    @GetMapping("/members/{name}")
+    public MemberResponse getMember(@PathVariable String name){
+        return memberService.readMember(name);
+    }
+
+    @PutMapping("/members/{name}")
+    public void updateMember(@PathVariable String name, @RequestBody UpdateMemberRequest updatedMember) {
+        memberService.updateMember(name, updatedMember);
+    }
+
+    @DeleteMapping("/members/{name}")
+    public void deleteMember(@PathVariable("name") String name) {
+        System.out.println(name + " DELETE name");
+        memberService.deleteMember(name);
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<String> handleMemberNotFoundException(MemberNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 }
+
