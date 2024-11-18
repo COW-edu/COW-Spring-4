3274990bcd3857f05f5b97e568ea5adb2ef71eca
Jungsukwoo <jungsw1106@naver.com>
2024-11-19 00:34:39 +0900

    변수 일치화

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/member/controller/dto/request/UpdateMemberRequest.java
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/member/controller/dto/request/UpdateMemberRequest.java
@@ -1,13 +1,12 @@
 package practice.jungsukwoocrudapi.member.controller.dto.request;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UpdateMemberRequest {
-    private String username;
+    private String name;
     private String email;
     private String password;
 }
