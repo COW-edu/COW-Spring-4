b92b02c0a07366f0187cbf8a33372e414731bc3a
Jungsukwoo <jungsw1106@naver.com>
2024-11-19 23:32:31 +0900

    게시글 작성 메소드 생성

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/post/controller/PostController.java
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/post/controller/PostController.java
@@ -1,16 +1,23 @@
 package practice.jungsukwoocrudapi.post.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import practice.jungsukwoocrudapi.post.controller.dto.request.CreatePostRequest;
 import practice.jungsukwoocrudapi.post.service.PostService;
 
+@RestController
+@RequestMapping("/posts")
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
-//    @PostMapping("/posts")
-//    public void makePost(@RequestBody CreatePostRequest createPostRequest) {
-//        postService.makePost(createPostRequest);
-//    }
+    @PostMapping()
+    public void postUp(@RequestBody CreatePostRequest createPostRequest) {
+        postService.postUp(createPostRequest);
+    }
 
 }
