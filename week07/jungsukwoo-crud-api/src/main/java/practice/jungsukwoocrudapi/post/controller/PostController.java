0c68b2a4f160ff8091fd1e9c88f93a2af6b79811
Jungsukwoo <jungsw1106@naver.com>
2024-11-20 00:37:18 +0900

    서비스로부터 업데이트 메소드 호출

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/post/controller/PostController.java
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/post/controller/PostController.java
@@ -2,11 +2,14 @@ package practice.jungsukwoocrudapi.post.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import practice.jungsukwoocrudapi.member.controller.dto.request.UpdateMemberRequest;
 import practice.jungsukwoocrudapi.member.controller.dto.response.MemberResponse;
 import practice.jungsukwoocrudapi.post.controller.dto.request.CreatePostRequest;
+import practice.jungsukwoocrudapi.post.controller.dto.request.UpdatePostRequest;
 import practice.jungsukwoocrudapi.post.controller.dto.response.PostResponse;
 import practice.jungsukwoocrudapi.post.service.PostService;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -27,8 +30,12 @@ public class PostController {
     }
 
     @GetMapping("/{id}")
-    public PostResponse getPostById(@PathVariable("id") Long id){
+    public PostResponse getPostById(@PathVariable("id") Long id) {
         return postService.getPostById(id);
     }
 
+    @PutMapping("/{id}")
+    public void updatePostById(@PathVariable("id") Long id, @RequestBody UpdatePostRequest updatePostRequest) {
+        postService.updatePostById(id, updatePostRequest);
+    }
 }
