af60a273afad1a296b8e473906d0f373ddefaa88
Jungsukwoo <jungsw1106@naver.com>
2024-11-20 00:06:43 +0900

    전체 조회, 특정 id 조회 메소드 호출

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/post/controller/PostController.java
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/post/controller/PostController.java
@@ -1,12 +1,13 @@
 package practice.jungsukwoocrudapi.post.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import practice.jungsukwoocrudapi.member.controller.dto.response.MemberResponse;
 import practice.jungsukwoocrudapi.post.controller.dto.request.CreatePostRequest;
+import practice.jungsukwoocrudapi.post.controller.dto.response.PostResponse;
 import practice.jungsukwoocrudapi.post.service.PostService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -18,6 +19,16 @@ public class PostController {
     @PostMapping()
     public void postUp(@RequestBody CreatePostRequest createPostRequest) {
         postService.postUp(createPostRequest);
+    }
+
+    @GetMapping
+    public List<PostResponse> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/{id}")
+    public PostResponse getPostById(@PathVariable("id") Long id){
+        return postService.getPostById(id);
     }
 
 }
