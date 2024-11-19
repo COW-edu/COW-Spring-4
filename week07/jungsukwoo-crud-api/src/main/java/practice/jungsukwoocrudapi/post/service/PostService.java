d7b96e85fadea1275ccf6795fa9fe41d5da6b603
Jungsukwoo <jungsw1106@naver.com>
2024-11-20 00:06:54 +0900

    전체 조회, 특정 id 조회 메소드 생성

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/post/service/PostService.java
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/post/service/PostService.java
@@ -4,11 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import practice.jungsukwoocrudapi.member.controller.dto.request.CreateMemberRequest;
+import practice.jungsukwoocrudapi.member.controller.dto.response.MemberResponse;
 import practice.jungsukwoocrudapi.member.entity.Member;
 import practice.jungsukwoocrudapi.member.repository.MemberRepository;
 import practice.jungsukwoocrudapi.post.controller.dto.request.CreatePostRequest;
+import practice.jungsukwoocrudapi.post.controller.dto.response.PostResponse;
 import practice.jungsukwoocrudapi.post.entity.Post;
 import practice.jungsukwoocrudapi.post.repository.PostRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +28,21 @@ public class PostService {
                 .orElseThrow(() -> new IllegalArgumentException("Member를 찾을 수 없습니다."));
         Post post = createPostRequest.toEntity(member);
         postRepository.save(post);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> getAllPosts() {
+        List<Post> posts = postRepository.findAll();  // 모든 게시글 조회
+        return posts.stream()
+                .map(PostResponse::from)  // 각 Post 엔티티를 PostResponse로 변환
+                .collect(Collectors.toList());  // 리스트로 수집
+    }
+
+    @Transactional(readOnly = true)
+    public PostResponse getPostById(final Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Post를 찾을 수 없습니다."));
+        return PostResponse.from(post);
     }
 
 
