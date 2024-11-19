87c8556b8c90b03fe46f6a595fa7e835a128d4db
Jungsukwoo <jungsw1106@naver.com>
2024-11-19 23:32:04 +0900

    게시글 작성 메소드 생성

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/post/service/PostService.java
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/post/service/PostService.java
@@ -2,20 +2,28 @@ package practice.jungsukwoocrudapi.post.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import practice.jungsukwoocrudapi.member.controller.dto.request.CreateMemberRequest;
+import practice.jungsukwoocrudapi.member.entity.Member;
+import practice.jungsukwoocrudapi.member.repository.MemberRepository;
+import practice.jungsukwoocrudapi.post.controller.dto.request.CreatePostRequest;
+import practice.jungsukwoocrudapi.post.entity.Post;
 import practice.jungsukwoocrudapi.post.repository.PostRepository;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PostService {
 
+    private final MemberRepository memberRepository;
     private final PostRepository postRepository;
 
-//    public void makePost(CreatePostRequest createPostRequest) {
-//        Post post = createPostRequest.toEntity();
-//        postRepository.savePost(post);
-//    }
-//
-//    public PostResponse readPost(String userId) {
-//        Post post = postRepository.readPost(userId);
-//    }
+    public void postUp(CreatePostRequest createPostRequest) {
+        Member member = memberRepository.findById(createPostRequest.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("Member를 찾을 수 없습니다."));
+        Post post = createPostRequest.toEntity(member);
+        postRepository.save(post);
+    }
+
+
 }
