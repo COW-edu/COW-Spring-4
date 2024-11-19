d22a5e6bf3be72eaf2b7f864edb2aa3c62395ac4
Jungsukwoo <jungsw1106@naver.com>
2024-11-20 01:33:08 +0900

    코드 컨벤션 적용

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/exception/MemberNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package practice.jungsukwoocrudapi.exception;
-
-public class MemberNotFoundException extends RuntimeException {
-    public MemberNotFoundException(String message) {
-        super(message);
-    }
-}
