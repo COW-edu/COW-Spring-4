5b4908d670aaaaf2aabea36ce18555975c5fca50
Jungsukwoo <jungsw1106@naver.com>
2024-11-21 01:04:54 +0900

    특정 post 조회 메소드 추가

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/comment/repository/CommentRepository.java
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/comment/repository/CommentRepository.java
@@ -3,8 +3,10 @@ package practice.jungsukwoocrudapi.comment.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import practice.jungsukwoocrudapi.comment.entity.Comment;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Optional<Comment> findbyId(Long id);
+    Optional<Comment> findById(Long id);
+    List<Comment> findByPostId(Long postId);
 }
