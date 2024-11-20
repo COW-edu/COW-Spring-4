1c10509f964f2e5e27552a74dd8053a2769178e4
Jungsukwoo <jungsw1106@naver.com>
2024-11-21 01:01:21 +0900

    읽어올 postId 받아오기

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/comment/dto/request/CreateCommentRequest.java
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/comment/dto/request/CreateCommentRequest.java
@@ -3,17 +3,20 @@ package practice.jungsukwoocrudapi.comment.dto.request;
 import lombok.Getter;
 import practice.jungsukwoocrudapi.comment.entity.Comment;
 import practice.jungsukwoocrudapi.member.entity.Member;
+import practice.jungsukwoocrudapi.post.entity.Post;
 
 @Getter
 public class CreateCommentRequest {
 
     private Long userId;
+    private Long postId;
     private String content;
 
-    public Comment toEntity(Member member){
+    public Comment toEntity(Member member, Post post){
         return Comment.builder()
                 .content(content)
                 .member(member)
+                .post(post)
                 .build();
     }
 }
