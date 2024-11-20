503f284df88b671f5ab32b1a30e59bd4d389d6a2
Jungsukwoo <jungsw1106@naver.com>
2024-11-21 00:57:17 +0900

    서비스에게 post 받아오기 요청

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/comment/Controller/CommentController.java
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/comment/Controller/CommentController.java
@@ -1,23 +1,27 @@
 package practice.jungsukwoocrudapi.comment.Controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import practice.jungsukwoocrudapi.comment.entity.Comment;
+import org.springframework.web.bind.annotation.*;
+import practice.jungsukwoocrudapi.comment.dto.request.CreateCommentRequest;
+import practice.jungsukwoocrudapi.comment.dto.response.CommentResponse;
 import practice.jungsukwoocrudapi.comment.service.CommentService;
-import practice.jungsukwoocrudapi.member.controller.dto.request.CreateMemberRequest;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/comments")
 @RequiredArgsConstructor
 public class CommentController {
 
-    private CommentService commentService;
+    private final CommentService commentService;
 
     @PostMapping()
-    public void commentUp(@RequestBody CreateMemberRequest createMemberRequest){
-        memberService.commentUp(createMemberRequest);
+    public void commentUp(@RequestBody CreateCommentRequest createCommentRequest){
+        commentService.commentUp(createCommentRequest);
+    }
+
+    @GetMapping("/post/{postId}")
+    public List<CommentResponse> getCommentsByPostId(@PathVariable Long postId) {
+        return commentService.getCommentsByPostId(postId);
     }
 }
