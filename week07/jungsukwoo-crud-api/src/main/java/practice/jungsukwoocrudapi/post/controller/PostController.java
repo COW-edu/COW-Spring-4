633b25d40dfe0c7708b70c292ee63d06edc5e7de
Jungsukwoo <jungsw1106@naver.com>
2024-11-21 00:50:24 +0900

    코드 컨벤션

--- week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/post/controller/PostController.java
+++ week07/jungsukwoo-crud-api/src/main/java/practice/jungsukwoocrudapi/post/controller/PostController.java
@@ -2,14 +2,11 @@ package practice.jungsukwoocrudapi.post.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import practice.jungsukwoocrudapi.member.controller.dto.request.UpdateMemberRequest;
-import practice.jungsukwoocrudapi.member.controller.dto.response.MemberResponse;
 import practice.jungsukwoocrudapi.post.controller.dto.request.CreatePostRequest;
 import practice.jungsukwoocrudapi.post.controller.dto.request.UpdatePostRequest;
 import practice.jungsukwoocrudapi.post.controller.dto.response.PostResponse;
 import practice.jungsukwoocrudapi.post.service.PostService;
 
-import java.security.Principal;
 import java.util.List;
 
 @RestController
